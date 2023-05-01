d7b604f21a9924c04587540cc2f0d5c35812e74e
Rafiere <rafapgbr@gmail.com>
2023-05-01 14:22:21 -0300

    feat: add lesson 01 exploring the DOM code

---
+++ lesson01-DOM/01-exploring-the-dom/lesson01.js
@@ -0,0 +1,16 @@
+/* What is JavaScript? */
+console.log("\nWhat is JavaScript?")
+
+/* O JavaScript foi criado em 1995 pelo Brendan Eich enquanto ele estava trabalhando no NetScape. */
+
+/* O HTML é sobre a estrutura da página, o CSS é sobre a estilização da página e o JS é sobre as
+* ações que ocorrem na página. */
+
+/* Document Object Model */
+console.log("\nDocument Object Model")
+
+/* O JavaScript, por padrão, cria um objeto que representa o DOM, ou seja, tudo o que está dentro
+* da página. */
+
+/* Abaixo, podemos acessar o título da página. */
+console.log(document.title)
